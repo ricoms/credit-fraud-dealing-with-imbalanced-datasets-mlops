af136d75dcfd1a8aee93720d94511b7dc7ec295a
Ricardo Savii <ricardosavii@gmail.com>
2020-12-04 17:58:16 -0300

    downgrade docker-compose version file, probably due to docker version

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-apt update && sudo apt install python3.8
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
